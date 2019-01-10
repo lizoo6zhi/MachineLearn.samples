43166d571fe35f5e49825f6f14ebb3aa1bb3ecff
lizoo6zhi <405997428@qq.com>
2019-01-10 14:46:15 +0800

    complate logistic

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import random
+import matplotlib.pyplot as plt
 
 def load_dataset():
 	"""
@@ -27,7 +28,7 @@ def grad_ascent(dataset,labels):
 	label_matrix = np.mat(labels).transpose()
 	row,col = np.shape(dataset_matrix)
 	weight = np.ones((col,1))
-	alpha = 0.001 #学习率
+	alpha = 0.01 #学习率
 	max_cycles = 500
 	for i in range(max_cycles):
 		h = sigmoid(dataset_matrix * weight)
@@ -44,10 +45,11 @@ def stoc_grad_ascent0(dataset,labels):
 	row,col = np.shape(dataset_matrix)
 	weights = np.ones(col)
 	alpha = 0.01
-	for i in range(row):
-		h = sigmoid(sum(dataset_matrix[i]*weights))
-		error = labels[i] - h
-		weights = weights + alpha*error*dataset_matrix[i]
+	for j in range(500):
+		for i in range(row):
+			h = sigmoid(sum(dataset_matrix[i]*weights))
+			error = labels[i] - h
+			weights = weights + alpha*error*dataset_matrix[i]
 	return weights
 	
 def stoc_grad_ascent1(dataset,labels,numiter=150):
@@ -68,7 +70,6 @@ def stoc_grad_ascent1(dataset,labels,numiter=150):
 	return weights
 	
 def plot_bestfit(weights):
-	import matplotlib.pyplot as plt
 	dataset,labels = load_dataset()
 	dataset_matrix = np.mat(dataset)
 	rows,cols = np.shape(dataset_matrix)
@@ -93,16 +94,63 @@ def plot_bestfit(weights):
 	plt.xlabel('x')
 	plt.ylabel('y')
 	plt.show()
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+	以下代码为测试
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+def stoc_grad_ascent0_heights_test(dataset,labels):
+	dataset_matrix = np.array(dataset)
+	row,col = np.shape(dataset_matrix)
+	weights = np.ones(col)
+	weights_iter_list = {}
+	alpha = 0.01
+	for j in range(500):
+		weights_iter_list[j] = []
+		for i in range(row):
+			h = sigmoid(sum(dataset_matrix[i]*weights))
+			error = labels[i] - h
+			weights = weights + alpha*error*dataset_matrix[i]
+			weights_iter_list[j].append(weights)
+	return weights,weights_iter_list
 	
-def array_matrix_t_test():
-	li = [1,2,3,4,5]
-	print('li:',li)
-	li_array = np.array(li)
-	print('li_array:',li_array)
-	li_matrix = np.mat(li_array)
-	print("li_matrix:",li_matrix)
-	li_matrix_t = li_matrix.transpose()
-	print('li_matrix_t:',li_matrix_t)
+def stoc_grad_ascent1_heights_test(dataset,labels,numiter=500):
+	dataset_matrix = np.array(dataset)
+	row,col = np.shape(dataset_matrix)
+	weights = np.ones(col)
+	weights_iter_list = {}
+	for j in range(numiter):
+		weights_iter_list[j] = []
+		dataindex = range(row)
+		for i in range(row):
+			alpha = 4 / (1.0+j+i)+0.01
+			randindex = int(random.uniform(0,len(dataindex)))
+			h = sigmoid(sum(dataset_matrix[randindex]*weights))
+			error = labels[randindex] - h
+			weights = weights + alpha*error*dataset_matrix[randindex]
+			weights_iter_list[j].append(weights)
+	return weights,weights_iter_list
+	
+def plot_heights_change(weights_iter_list):
+	x = []
+	y0 = []
+	y1 = []
+	y2 = []
+	for i,weights_list in weights_iter_list.items():
+		for wieghts in weights_list:
+			x.append(i)
+			y0.append(wieghts[0])
+			y1.append(wieghts[1])
+			y2.append(wieghts[2])
+	plt.subplot(311)
+	plt.ylabel('w0')
+	plt.plot(x,y0)
+	plt.subplot(312)
+	plt.ylabel('w1')
+	plt.plot(x,y1)
+	plt.subplot(313)
+	plt.ylabel('w2')
+	plt.plot(x,y2)
+	plt.show()
 	
 if __name__ == "__main__":
 	dataset,labels = load_dataset()
@@ -112,8 +160,10 @@ if __name__ == "__main__":
 	
 	A2 = stoc_grad_ascent0(dataset,labels)
 	plot_bestfit(A2)
-	
+	heights,weights_iter_list = stoc_grad_ascent0_heights_test(dataset,labels)
+	plot_heights_change(weights_iter_list)
 	A3 = stoc_grad_ascent1(dataset,labels)
 	plot_bestfit(A3)
-	
+	heights,weights_iter_list = stoc_grad_ascent1_heights_test(dataset,labels)
+	plot_heights_change(weights_iter_list)
 	
--- matplot.py
+++ matplot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 def calcAB(x,y):
 	"""
@@ -15,30 +16,45 @@ def calcAB(x,y):
 	a = (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX)
 	b = (sumXX * sumY - sumX * sumXY) / (n * sumXX - sumX * sumX)
 	return a, b
+
+def test_calcAB():
+	xi = [1,2,3,4,5,6,7,8,9,10]
+	yi = [10,11.5,12,13,14.5,15.5,16.8,17.3,18,18.7]
+	a,b=calcAB(xi,yi)
+	print("y = %10.5fx + %10.5f" %(a,b))
+
+	x = np.linspace(0,10)
+	y = a * x + b
+	plt.plot(x,y)
+	plt.scatter(xi,yi)
+	plt.show()
+
+def muldim():	
+	x = [1,2,3,4,5,6,7,8,9,10]
+	y = [10,11.5,12,13,14.5,15.5,16.8,17.3,18,18.7]
+	X = np.vstack([np.ones(len(x)),x]).T
+	Y = np.array(y).T
+	W=np.dot(np.matrix(np.dot(X.T,X))**-1,np.dot(X.T,Y))
+	yi=np.dot(X,W.T)#这里公式里是不需要转置的，但由于矩阵运算时W自动保存成一行多列的矩阵，所以多转置一下，配合原公式的计算。
+	print(X)
+	print(Y)
+	print(W)
+	print(yi)#拟合出的预测点
+	plt.plot(x,y,'o',label='data',markersize=10)
+	plt.plot(x,yi,'r',label='line')
+	plt.show()
+
+def plot_sigmoid():
+	x1 = np.linspace(-10,10)
+	y1 = 1 / (1+np.exp(-x1))
+	x2 = np.linspace(-60,60)
+	y2 = 1 / (1+np.exp(-x2))
+	fig = plt.figure()
+	ax = fig.add_subplot(211)
+	ax.plot(x1,y1)
+	ax = fig.add_subplot(212)
+	ax.plot(x2,y2)
+	plt.show()
 	
-xi = [1,2,3,4,5,6,7,8,9,10]
-yi = [10,11.5,12,13,14.5,15.5,16.8,17.3,18,18.7]
-a,b=calcAB(xi,yi)
-print("y = %10.5fx + %10.5f" %(a,b))
-
-x = np.linspace(0,10)
-y = a * x + b
-plt.plot(x,y)
-plt.scatter(xi,yi)
-plt.show()
-
-
-
-x = [1,2,3,4,5,6,7,8,9,10]
-y = [10,11.5,12,13,14.5,15.5,16.8,17.3,18,18.7]
-X = np.vstack([np.ones(len(x)),x]).T
-Y = np.array(y).T
-W=np.dot(np.matrix(np.dot(X.T,X))**-1,np.dot(X.T,Y))
-yi=np.dot(X,W.T)#这里公式里是不需要转置的，但由于矩阵运算时W自动保存成一行多列的矩阵，所以多转置一下，配合原公式的计算。
-print(X)
-print(Y)
-print(W)
-print(yi)#拟合出的预测点
-plt.plot(x,y,'o',label='data',markersize=10)
-plt.plot(x,yi,'r',label='line')
-plt.show()
+if __name__ == '__main__':
+	plot_sigmoid()
