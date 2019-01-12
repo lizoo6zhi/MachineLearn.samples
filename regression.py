a57f72f1537dd946fec2ebda6c49a8b9f33a1382
lizoo6zhi <405997428@qq.com>
2019-01-12 18:06:34 +0800

    add ridge_regres

--- regression.py
+++ regression.py
@@ -52,7 +52,54 @@ def lwrw(test_point, xarray, yarray, k = 1.0):
 	else:
 		temp = temp.I * mat_x.T * weights * mat_y
 	return test_point*temp
-		
+
+def data_normalize(dataset):
+	"""
+	数据标准化处理
+	"""
+	dataset_mat = np.mat(dataset)
+	mean = np.mean(dataset_mat,0) #计算每行平均值
+	print('mat mean:',mean)
+	var = np.var(dataset,0) #计算每行标准差
+	print('mat var:',var)
+	return mean,var
+	
+def ridge_regres(xarray,yarray,lam=0.2):
+	"""
+	岭回归
+	处理当特征比样本点还多，也酒水说输入数据的矩阵X不是满秩矩阵，非满秩矩阵在求逆时会出现问题
+	w = (X.T*X + rI).I * X.T * Y
+	"""
+	xmat = np.mat(xarray)
+	ymat = np.mat(yarray).T
+	xtx = xmat.T * xmat
+	denom = xtx + np.eye(np.shape(xmat)[1])*lam
+	if np.linalg.det(denom) == 0.0:
+		print('denom 没有逆矩阵')
+	ws = denom.I * xmat.T * ymat
+	return ws
+	
+def ridge_test(xarray,yarray,ax):
+	xmat = np.mat(xarray)
+	row,col = np.shape(xmat)
+	ymat = np.mat(yarray)
+	xmean,xvar = data_normalize(xarray) #对数据进行标准化处理
+	np.seterr(divide='ignore',invalid='ignore')
+	xmat = (xmat - xmean) / xvar
+	num_test_pts = 30
+	wmat = np.ones((num_test_pts,col))
+	
+	lam_list = []
+	for i in range(num_test_pts):
+		lam = np.exp(i-10)
+		lam_list.append(i-10)
+		ws = ridge_regres(xarray,yarray,lam)
+		wmat[i,:] = ws.T
+	
+	#绘制不同lam情况下参数变化情况
+	for j in range(np.shape(wmat)[1]):
+		ax.plot(lam_list,wmat[:,j])
+	
 def plot_stand_regress(dataset,label,ws,ax):
 	"""
 	绘制数据集和拟合曲线
@@ -61,6 +108,7 @@ def plot_stand_regress(dataset,label,ws,ax):
 	x = dataset_matrix[:,1]
 	y = label
 	cal_y = dataset * ws
+	print(np.corrcoef(cal_y.T,np.mat(y)))
 	ax.scatter(x.T.getA(),y,s=10,c='red')
 	ax.plot(x,cal_y)
 	
@@ -76,13 +124,14 @@ def plot_lwrw_regress(dataset,label,cal_y_list,ax):
 	sorted_index = x.argsort(0)
 	sorted_x = dataset_matrix[sorted_index][:,0,:]
 	ax.plot(sorted_x[:,1],cal_y_list[sorted_index])
-	
+
+		
 if __name__ == "__main__":
 	dataset,label = load_dataset('dataset/ex0.txt')
 	fig = plt.figure()
 	# 标准线性回归函数测试
 	stand_ws = stand_regres(dataset,label)
-	ax = fig.add_subplot(311)
+	ax = fig.add_subplot(321)
 	ax.text(0.05,4.2,'stand')
 	plot_stand_regress(dataset,label,stand_ws,ax)
 	
@@ -94,7 +143,7 @@ if __name__ == "__main__":
 	for row in range(rows):
 		cal_y = lwrw(dataset[row],dataset,label,0.01)
 		x_y_map[row] = cal_y
-	ax = fig.add_subplot(312)
+	ax = fig.add_subplot(323)
 	ax.text(0.05,4.2,'k=0.01')
 	plot_lwrw_regress(dataset,label,x_y_map,ax)
 	
@@ -102,8 +151,18 @@ if __name__ == "__main__":
 	for row in range(rows):
 		cal_y = lwrw(dataset[row],dataset,label,0.003)
 		x_y_map[row] = cal_y
-	ax = fig.add_subplot(313)
+	ax = fig.add_subplot(325)
 	ax.text(0.05,4.2,'k=0.003')
 	plot_lwrw_regress(dataset,label,x_y_map,ax)
+	
+	#岭回归测试
+	ws = ridge_regres(dataset,label)
+	ax = fig.add_subplot(322)
+	ax.text(0.05,4.2,'lam=0.2')
+	plot_stand_regress(dataset,label,ws,ax)
+	
+	ax = fig.add_subplot(324)
+	ax.text(10,2.5,'log(r)-->w')
+	ridge_test(dataset,label,ax)
 	
 	plt.show()
