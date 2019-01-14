17dd15c59e70a989ad4be1bc873e8111b7e0ebed
lizoo6zhi <405997428@qq.com>
2019-01-14 17:55:24 +0800

    complete regression.py

--- regression.py
+++ regression.py
@@ -11,8 +11,9 @@ def load_dataset(file):
 	with open(file,'r') as pf:
 		for line in pf:
 			line_list = line.strip().split('\t')
-			dataset.append([float(data) for data in line_list[0:2]])
-			label.append(float(line_list[-1][0:-1]))
+			n = len(line_list)
+			dataset.append([float(data) for data in line_list[0:-1]])
+			label.append(float(line_list[-1]))
 	return dataset,label
 	
 def stand_regres(dataset,label):
@@ -55,13 +56,11 @@ def lwrw(test_point, xarray, yarray, k = 1.0):
 
 def data_normalize(dataset):
 	"""
-	数据标准化处理
+	数据标准化处data_normalize理
 	"""
 	dataset_mat = np.mat(dataset)
 	mean = np.mean(dataset_mat,0) #计算每行平均值
-	print('mat mean:',mean)
 	var = np.var(dataset,0) #计算每行标准差
-	print('mat var:',var)
 	return mean,var
 	
 def ridge_regres(xarray,yarray,lam=0.2):
@@ -125,14 +124,51 @@ def plot_lwrw_regress(dataset,label,cal_y_list,ax):
 	sorted_x = dataset_matrix[sorted_index][:,0,:]
 	ax.plot(sorted_x[:,1],cal_y_list[sorted_index])
 
-		
+def stage_wise(dataset,label,step,numiter=100):
+	"""
+	前向逐步回归
+	"""
+	xmat = np.mat(dataset)
+	xmean,xvar = data_normalize(dataset) #平均值和标准差
+	ymat = np.mat(label).T
+	ymat = ymat - np.mean(ymat,0)
+	xmat = (xmat - xmean) / xvar
+	m,n = np.shape(xmat)
+	return_mat = np.zeros((numiter,n))
+	ws = np.zeros((n,1))
+	ws_test = ws.copy()
+	ws_max = ws.copy()
+	
+	for iter in range(numiter):  #迭代次数
+		lowest_error = np.inf
+		for j in range(n):  #对每个特征进行变量
+			for sign in [-1,1]:
+				ws_test = ws.copy()
+				ws_test[j] += step*sign
+				ytest = xmat * ws_test
+				rssE = ((ymat.A-ytest.A)**2).sum()
+				if rssE < lowest_error:
+					lowest_error = rssE
+					ws_max = ws_test
+		ws = ws_max.copy()
+		return_mat[iter,:] = ws.T
+	return return_mat	
+
+def stage_wise_test(ws_mat,iternum,ax):
+	"""
+	前向逐步回归算法中回归系数随迭代次数的变化情况
+	"""	
+	x = range(iternum)
+	for i in range(np.shape(ws_mat)[1]):
+		ax.plot(x,ws_mat[:,i])
+					
 if __name__ == "__main__":
 	dataset,label = load_dataset('dataset/ex0.txt')
 	fig = plt.figure()
 	# 标准线性回归函数测试
 	stand_ws = stand_regres(dataset,label)
 	ax = fig.add_subplot(321)
-	ax.text(0.05,4.2,'stand')
+	ax.text(0.05,4.2,'stand_regres')
 	plot_stand_regress(dataset,label,stand_ws,ax)
 	
 	#局部加权回归函数测试
@@ -144,7 +180,7 @@ if __name__ == "__main__":
 		cal_y = lwrw(dataset[row],dataset,label,0.01)
 		x_y_map[row] = cal_y
 	ax = fig.add_subplot(323)
-	ax.text(0.05,4.2,'k=0.01')
+	ax.text(0.05,4.2,'lwrw k=0.01')
 	plot_lwrw_regress(dataset,label,x_y_map,ax)
 	
 	#当k为0.003
@@ -152,17 +188,35 @@ if __name__ == "__main__":
 		cal_y = lwrw(dataset[row],dataset,label,0.003)
 		x_y_map[row] = cal_y
 	ax = fig.add_subplot(325)
-	ax.text(0.05,4.2,'k=0.003')
+	ax.text(0.05,4.2,'lwrw k=0.003')
 	plot_lwrw_regress(dataset,label,x_y_map,ax)
 	
 	#岭回归测试
 	ws = ridge_regres(dataset,label)
 	ax = fig.add_subplot(322)
-	ax.text(0.05,4.2,'lam=0.2')
+	ax.text(0.05,4.2,'ridge_regres lam=0.2')
 	plot_stand_regress(dataset,label,ws,ax)
 	
+	dataset,label = load_dataset('dataset/abalone.txt')
 	ax = fig.add_subplot(324)
-	ax.text(10,2.5,'log(r)-->w')
+	ax.text(10,2.5,'ridge_regres log(r)-->w')
 	ridge_test(dataset,label,ax)
 	
+	#前向逐步回归
+	ax = fig.add_subplot(326)
+	
+	ax.text(300,-0.4,'stage_wise 500')
+	ws = stage_wise(dataset,label,0.01,500)
+	print("前向逐步回归经过迭代后的最后回归系数：",ws[-1,:])
+	stage_wise_test(ws,500,ax)
+	
+	#使用标准回归对"dataset/abalone.txt"数据集进行测试
+	
+	#对数据进行标准化处理
+	xman,xvar = data_normalize(dataset)
+	xmat = np.mat(dataset)
+	xmat = (xmat - xman) / xvar
+	ymat = np.mat(label)
+	ax = stand_regres(xmat,ymat)
+	print("标准回归后得到的回归系数：",ws[-1,:])
 	plt.show()
